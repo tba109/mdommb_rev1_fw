ec34eacc3b754ddac6cc383e5037f86517d078dd
Aaron Fienberg <atfienberg@psu.edu>
2020-11-12 18:57:31 -0500

    do not register io_reset in adc_discr_chan

--- hdl/adc_discr_channel/adc_discr_channel.v
+++ hdl/adc_discr_channel/adc_discr_channel.v
@@ -23,8 +23,8 @@ module adc_discr_channel
    input discr_bitslip,
    input discr_io_reset,
 
-   output reg[11:0] adc_bits,
-   output reg[7:0]  discr_bits,
+   output reg[11:0] adc_bits = 0,
+   output reg[7:0]  discr_bits = 0,
 
    input[4:0] delay_tap_in_0,
    input[4:0] delay_tap_in_1,
@@ -32,15 +32,6 @@ module adc_discr_channel
    output[4:0] delay_tap_out_0,
    output[4:0] delay_tap_out_1
   );
-
-  (* DONT_TOUCH = "true" *) reg[1:0] i_adc_io_reset;
-  (* DONT_TOUCH = "true" *) reg i_discr_io_reset;
-  always @(posedge lclk) begin
-    i_adc_io_reset <= adc_io_reset;
-  end
-  always @(posedge discr_fclk) begin
-    i_discr_io_reset <= discr_io_reset;
-  end
 
   wire[5:0] adc_bits_0;
   ADC_SERDES ADC_SERDES_0
@@ -56,7 +47,7 @@ module adc_discr_channel
      .bitslip(adc_bitslip[0]),
      .clk_in(adc_dclk),
      .clk_div_in(lclk),
-     .io_reset(i_adc_io_reset[0])
+     .io_reset(adc_io_reset[0])
    );
 
   wire[5:0] adc_bits_1;
@@ -73,7 +64,7 @@ module adc_discr_channel
      .bitslip(adc_bitslip[1]),
      .clk_in(adc_dclk),
      .clk_div_in(lclk),
-     .io_reset(i_adc_io_reset[1])
+     .io_reset(adc_io_reset[1])
    );
   wire[11:0] i_adc_bits_0 = {adc_bits_1, adc_bits_0};
 
@@ -85,11 +76,11 @@ module adc_discr_channel
    .bitslip(discr_bitslip),
    .clk_in(discr_dclk),
    .clk_div_in(discr_fclk),
-   .io_reset(i_discr_io_reset)
+   .io_reset(discr_io_reset)
   );
 
-  reg[11:0] i_adc_bits_1;
-  reg[7:0] i_discr_bits_1;
+  reg[11:0] i_adc_bits_1 = 0;
+  reg[7:0] i_discr_bits_1 = 0;
   always @(posedge lclk) begin
     i_adc_bits_1 <= i_adc_bits_0;
     adc_bits <= i_adc_bits_1;
